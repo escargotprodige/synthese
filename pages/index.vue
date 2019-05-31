2013cbc779c9eb10d059266a1d4e4bb76f1bbb1a
Zenon Roy <king-zed@hotmail.com>
2019-05-31 00:58:39 -0400

    selection de l'item (marque/type/rareté)

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,23 @@
       <v-card>
         <v-card-text>
           <h3>Sélection de l'item</h3>
-          <v-select label="Marque"></v-select>
-          <v-select label="Type"></v-select>
-          <v-select label="Rareté"></v-select>
-          <v-slider label="Niveau" :min="1" :max="72" thumb-label="always" always-dirty></v-slider>
+          <v-select label="Marque" :items="brands" item-text="name" v-model="brand"></v-select>
+          <v-select label="Type" :items="types" v-model="type"></v-select>
+          <v-select
+            label="Rareté"
+            :items="rarities"
+            item-text="text"
+            item-value="value"
+            v-model="rarity"
+          ></v-select>
+          <v-slider
+            label="Niveau"
+            :min="1"
+            :max="72"
+            thumb-label="always"
+            always-dirty
+            v-model="level"
+          ></v-slider>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -40,4 +53,42 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  data() {
+    return {
+      brand: 'Hyperion',
+      level: 22,
+      type: 'PISTOL',
+      rarity: 'COMMON',
+      types: [
+        { text: 'Pistol', value: 'PISTOL' },
+        { text: 'Shotgun', value: 'SHOTGUN' },
+        { text: 'Assault rifle', value: 'ASSAULT_RIFLE' },
+        { text: 'Sniper', value: 'SNIPER' },
+        { text: 'Rocket Launcher', value: 'ROCKET_LAUNCHER' },
+        { text: 'SMG', value: 'SMG' },
+      ],
+      rarities: [
+        { text: 'Commun', value: 'COMMON' },
+        { text: 'Non commun', value: 'UNCOMMON' },
+        { text: 'Rare', value: 'RARE' },
+        { text: 'Tres rare', value: 'VERY_RARE' },
+      ],
+    }
+  },
+  apollo: {
+    brands: gql`
+      query {
+        brands {
+          name
+        }
+      }
+    `,
+  },
+}
+</script>
 
